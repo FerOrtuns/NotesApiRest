1bca34f43bc41728120dc5f52c2908fa33f5f1fc
FerOrtuns <Fernando.OrtunoLopez@gmail.com>
2024-03-15 12:22:03 +0100

    commit todo ok antes de incluir bbdd

--- src/main/java/pue/edu/notesrestapi/api/UserController.java
+++ src/main/java/pue/edu/notesrestapi/api/UserController.java
@@ -5,16 +5,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pue.edu.notesrestapi.domain.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class UserController {
 
+    private List<User> users;
+
+    public UserController() {
+
+        users = List.of(new User("Fernando", "Fer@gmail.com"),
+                new User("Cris", "Cris@gmail.com"),
+                new User("Sthep", "Sthep@gmail.com"));
+    }
+
     @GetMapping("/users/{id}")
     public User getUser(@PathVariable long id) {
-        return new User("Fernando", "fer@gmaail.com");
+        return users.stream()
+                .filter(user -> user.getId() == id)
+                .findFirst()
+                .get();
     }
 
     @GetMapping("/hello")
     public String getHello() {
         return "Hello world, amigos del curso de Java :)";
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return users;
     }
 }
